cc145755e19f29dfe093a1efe905ea2314ae46c8
심소정 <opmiu@mz.co.kr>
2019-06-24 14:13:14 +0900

    폴더 구조 생성중

---
+++ src/arteon/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1>test</h1>
+  </section>
+</template>
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  border: 1px solid red;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,23 +1,26 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+import Launching from './arteon/Index.vue';
+
+Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: Launching
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
-})
+});
