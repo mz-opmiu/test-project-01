3f09b25d8124ab5505292cff1b5821da833b7fee
심소정 <opmiu@mz.co.kr>
2019-07-02 16:31:03 +0900

    중간 커밋

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
+import '@babel/polyfill';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 import 'expose-loader?$!expose-loader?jQuery!jquery';
-import '@babel/polyfill';
+
+import '@/styles/base/_reset-eric.scss';
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -5,14 +5,12 @@ module.exports = {
     loaderOptions: {
       sass: {
         data: `
-                @import "@/styles/base/_reset-eric.scss";
                 @import url('https://fonts.googleapis.com/css?family=Noto+Sans&display=swap');
+                @import "@/styles/page/_reset.scss";
                 @import "@/styles/vars/_variables.scss";
                 @import "@/styles/mixin/_background.scss";
                 @import "@/styles/util/_a11y-hidden.scss";
                 @import "@/styles/page/_layout.scss";
-                @import "@/styles/page/_reset.scss";
-
               `
       }
     }
